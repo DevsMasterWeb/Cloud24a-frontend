c7bd6627599e55b35ed7ff5ded1e318d88eeb92d
Sushant Subedi <subedisushant781@gmail.com>
2026-05-05 08:35:05 +0545

    first commit

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export type QueryItem = {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  query: string;
+};
+
+export const BACKEND_API_URL =
+  "https://sushant-24-cloud-backend-gva2bwdrc6gtb3ea.southeastasia-01.azurewebsites.net//queries";
+
+export const APP_API_URL = "/api/queries";
